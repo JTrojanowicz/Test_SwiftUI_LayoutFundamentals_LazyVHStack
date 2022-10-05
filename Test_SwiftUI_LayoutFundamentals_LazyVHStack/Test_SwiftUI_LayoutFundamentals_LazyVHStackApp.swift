0daba41dd017db26199d990f98ba9af811425fe5
JTrojanowicz <jaroslaw.trojanowicz@gmail.com>
2022-10-05 11:43:35 +0200

    Initial Commit

---
+++ Test_SwiftUI_LayoutFundamentals_LazyVHStack/Test_SwiftUI_LayoutFundamentals_LazyVHStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUI_LayoutFundamentals_LazyVHStackApp.swift
+//  Test_SwiftUI_LayoutFundamentals_LazyVHStack
+//
+//  Created by Jaroslaw Trojanowicz on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUI_LayoutFundamentals_LazyVHStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
